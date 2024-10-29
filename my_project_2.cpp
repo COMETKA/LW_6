4fc72bad84963ff77beafa25b651eb65f326c0e6
Yaroslav <aroslavmikirticev@gmail.com>
2024-10-29 23:38:33 +0200

    first commit

---
+++ my_project_2.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+    cout << showpos << setw(9) << 9 << endl;
+    cout << setw(15) << fixed << setprecision(3) << internal << -67.09124 << endl;
+    cout << showpos << setw(9) << right << 643 << endl;
+    cout << hex << 2209 << dec << endl;
+    cout << fixed << setprecision(2) << -111.0 << endl;
+    cout << 1 << 43 << endl;
+    cout << "Hello WORLD!!!"
+    
+
+}
+    
