e7022c07992c0af6f4d570438d8cfb867db43473
Yaroslav <aroslavmikirticev@gmail.com>
2024-11-05 13:23:02 +0200

    second commit

--- my_project_2.cpp
+++ my_project_2.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 int main()
 {
-    cout << showpos << setw(20) << 4 << endl;
-    cout << setw(20) << fixed << setprecision(4) << internal << 62.09 << endl;
-    cout << showpos << setw(20) << right << -14 << endl;
-    cout << hex << 2209 << endl;
-    cout << setprecision(3) << -11.0 << endl;
+    cout << showpos << setw(15) << 4 << endl;
+    cout << setw(15) << fixed << setprecision(7) << internal << 62.095323 << endl;
+    cout << showpos << setw(15) << right << 25 << endl;
+    cout << hex << 2209 << dex << endl;
+    cout << setprecision(3) << internal << -11.0 << endl;
     cout << 4 << endl;
     
     return 0;
