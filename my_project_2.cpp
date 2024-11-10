3199f90e3666a36465e3b4c97336177be40e93f3
Yaroslav <aroslavmikirticev@gmail.com>
2024-11-10 23:03:25 +0200

    new changes

--- my_project_2.cpp
+++ my_project_2.cpp
@@ -5,12 +5,12 @@
 using namespace std;
 int main()
 {
-    cout << showpos << setw(10) << 4 << endl;
-    cout << setw(10) << fixed << setprecision(7) << internal << 62.0323 << endl;
-    cout << showpos << setw(15) << right << 13 << endl;
-    cout << hex << 3309 << dex << endl;
-    cout << setprecision(3) << internal << -11.00 << endl;
-    cout << 4 << endl;
+    cout << showpos << setw(15) << 6 << endl;
+    cout << setw(15) << fixed << setprecision(7) << internal << 62.032343 << endl;
+    cout << showpos << setw(10) << right << 11 << endl;
+    cout << hex << 3409 << dex << endl;
+    cout << setprecision(3) << internal << -11.0000 << endl;
+    cout << 6 << endl;
     
     return 0;
     
